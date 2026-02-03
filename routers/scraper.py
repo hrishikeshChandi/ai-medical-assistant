298ca1c6018e82a66c23fe0d5a612fac20e20adf
Hrishikesh Chandi <143275248+hrishikeshChandi@users.noreply.github.com>
2026-02-03 14:03:01 +0530

    Remove commented job status endpoint from scraper.py
    Removed commented-out job status endpoint code.

--- routers/scraper.py
+++ routers/scraper.py
@@ -22,37 +22,3 @@ async def get_hospital_data(city: str):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=str(e),
         )
-
-
-# @router.get("/job_status/{job_id}", status_code=status.HTTP_200_OK)
-# async def get_job_status(job_id: str):
-#     try:
-#         job = queue.fetch_job(job_id)
-#         if not job:
-#             raise HTTPException(
-#                 status_code=status.HTTP_404_NOT_FOUND,
-#                 detail="job not found, please check your job id and try again.",
-#             )
-#         if job.is_finished:
-#             result = job.result
-#             if result["success"]:
-#                 return {
-#                     "status": "completed",
-#                     "data": result,
-#                 }
-#             else:
-#                 raise HTTPException(
-#                     status_code=result["status_code"],
-#                     detail=result["message"],
-#                 )
-#         elif job.is_queued:
-#             return {"status": "queued"}
-#         elif job.is_started:
-#             return {"status": "in progress"}
-#         else:
-#             return {"status": "failed"}
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#             detail=str(e),
-#         )
